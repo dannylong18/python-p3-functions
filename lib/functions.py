d26ff87a5e32b4156f0dec18c850abf60b94ff0b
Daniel Long <danielwlong618@gmail.com>
2024-02-06 21:27:46 -0600

    lab done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
+
+greet('Naureen')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2 
+
+add(45, 55)
 
 def halve(number):
-    pass
+    return number/2
+halve(100)
